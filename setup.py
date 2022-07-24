5bf38c88aa5a612347c9c048c8d00ddbc7d81aae
German Martinez Solis <german.mtz.solis@gmail.com>
2022-07-23 20:03:56 -0500

    Update setup (#2)
    * fixed typo
    
    * updated setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import find_packages, setup
 setup(
     name='proxynova_scrapper',
     packages=find_packages(),
-    version='0.1',
+    version='0.2',
     description='Package to get proxies from proxynova.com via web scraping',
     author='German Martinez Solis',
+    author_email='german.mtz.solis@gmail.com',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     license='MIT',
@@ -12,9 +13,10 @@ setup(
         "Bug Tracker": "https://github.com/GermanMtzmx/proxynova_scraper",
     },
     classifiers=[
-        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     install_requires=['bs4', 'html5lib', 'pyppeteer', 'asyncio', 'unidecode'],
+    python_requires='>=3',
 )
